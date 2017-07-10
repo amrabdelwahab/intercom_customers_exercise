80acc1429caee8c5229d7c9b002964b2e76191a9
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-10 20:55:31 +0200

    Add a failing spec to test for the customer initialize method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,7 +3,19 @@ require './lib/models/customer'
 
 describe Models::Customer do
   let(:attrs) { {} }
+  subject { described_class.new(attrs) }
+
   describe '#initialize' do
-    
+    let(:attrs) do
+      {
+        latitude: '52.986375',
+        user_id: 12,
+        name: 'Christina McArdle',
+        longitude: '-6.043701'
+      }
+    end
+
+    specify { expect(subject.user_id).to eq 12 }
+    specify { expect(subject.name).to eq 'Christina McArdle' }
   end
 end
