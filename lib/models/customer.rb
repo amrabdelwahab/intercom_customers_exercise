cdee1e3b08790e8f395de41804d0ab8ed1cabda0
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-10 21:06:13 +0200

    Add location model and location instance variable to customer

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -1,10 +1,12 @@
+require './lib/models/location'
 module Models
   class Customer
-    attr_reader :user_id, :name
+    attr_reader :user_id, :name, :location
 
     def initialize(attrs)
       @user_id = attrs[:user_id]
       @name = attrs[:name]
+      @location = Location.new(attrs[:longitude], attrs[:latitude])
     end
   end
 end
---
+++ lib/models/location.rb
@@ -0,0 +1,8 @@
+module Models
+  class Location
+    def initialize(longitude, latitude)
+      @longitude = longitude
+      @latitude = latitude
+    end
+  end
+end
