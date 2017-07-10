ba3a19d4e6c7bbe88d6a7d5977076f3ca469b108
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-10 21:02:40 +0200

    Add an example that asserts creating a location instance for every customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -17,5 +17,6 @@ describe Models::Customer do
 
     specify { expect(subject.user_id).to eq 12 }
     specify { expect(subject.name).to eq 'Christina McArdle' }
+    specify { expect(subject.location).to be_a Models::Location }
   end
 end
