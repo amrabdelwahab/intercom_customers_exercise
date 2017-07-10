2d8bea32bb9e462db28e2fdac7fd1b304228141f
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-10 21:35:13 +0200

    Implement comparable on the customer model
    * include the comparable mixin
    * implement the method <=>

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -1,12 +1,17 @@
 require './lib/models/location'
 module Models
   class Customer
+    include Comparable
     attr_reader :user_id, :name, :location
 
     def initialize(attrs)
       @user_id = attrs[:user_id]
       @name = attrs[:name]
       @location = Location.new(attrs[:longitude], attrs[:latitude])
+    end
+
+    def <=>(other)
+      user_id <=> other.user_id
     end
   end
 end
