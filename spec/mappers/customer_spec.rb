06b7c08482fc65e42d65abcb521b42e3b65e4418
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-11 20:03:32 +0200

    Add a spec to test that the customer_mapper maps the customer location correctly

--- spec/mappers/customer_spec.rb
+++ spec/mappers/customer_spec.rb
@@ -14,6 +14,12 @@ describe Mappers::Customer do
         expect(subject.customer.user_id).to eq 1
         expect(subject.customer.name).to eq 'Christina McArdle'
       end
+
+      it 'should map the customer location attributes correctly' do
+        expect(subject.customer.location).to be_a Models::Location
+        expect(subject.customer.location.longitude).to eq '-6.043701'
+        expect(subject.customer.location.latitude).to eq '52.986375'
+      end
     end
   end
 end
