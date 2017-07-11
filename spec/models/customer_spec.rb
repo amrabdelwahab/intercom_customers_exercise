b76213094d55ed4a131c34d1ba753096e0066f31
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-11 15:57:23 +0200

    Change the customer model spec to expect string keys hash

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,10 +4,10 @@ require './lib/models/customer'
 describe Models::Customer do
   let(:attrs) do
     {
-      latitude: '52.986375',
-      user_id: 12,
-      name: 'Christina McArdle',
-      longitude: '-6.043701'
+      'latitude' => '52.986375',
+      'user_id' => 12,
+      'name' => 'Christina McArdle',
+      'longitude' => '-6.043701'
     }
   end
   subject { described_class.new(attrs) }
@@ -20,8 +20,8 @@ describe Models::Customer do
 
   describe '#<=>' do
     context 'when two customers have the same user_id' do
-      let(:customer1) { described_class.new(user_id: 12) }
-      let(:customer2) { described_class.new(user_id: 12) }
+      let(:customer1) { described_class.new('user_id' => 12) }
+      let(:customer2) { described_class.new('user_id' => 12) }
 
       it 'should equate the two customers' do
         expect(customer1).to eq customer2
@@ -29,8 +29,8 @@ describe Models::Customer do
     end
 
     context 'when two customers have different user_id' do
-      let(:customer1) { described_class.new(user_id: 1) }
-      let(:customer2) { described_class.new(user_id: 12) }
+      let(:customer1) { described_class.new('user_id' => 1) }
+      let(:customer2) { described_class.new('user_id' => 12) }
 
       it 'should evaluate </> than of the customers based on the user_id' do
         expect(customer1 < customer2).to be_truthy
@@ -38,9 +38,9 @@ describe Models::Customer do
     end
 
     context 'when you have an array of customers' do
-      let(:customer1) { described_class.new(user_id: 1) }
-      let(:customer2) { described_class.new(user_id: 10) }
-      let(:customer3) { described_class.new(user_id: 20) }
+      let(:customer1) { described_class.new('user_id' => 1) }
+      let(:customer2) { described_class.new('user_id' => 10) }
+      let(:customer3) { described_class.new('user_id' => 20) }
       let(:customers) { [customer2, customer1, customer3] }
 
       it 'should sort an array of customers based on user_id using #sort' do
