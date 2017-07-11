8b32e37128c5b51ded88e4da99c353d4e48ec3d8
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-11 15:33:31 +0200

    Add a spec to the customer_repository to handle the case of empty array of data from the file reader

--- spec/respositories/customer_spec.rb
+++ spec/respositories/customer_spec.rb
@@ -3,4 +3,16 @@ require './lib/repositories/customer'
 
 describe Repositories::Customer do
   subject { described_class.new }
+  let(:json_data) { [] }
+  before do
+    allow_any_instance_of(Services::FileReader)
+      .to receive(:json_array)
+      .and_return(json_data)
+  end
+
+  describe '#all' do
+    context 'when the file has no valid records' do
+      specify { expect(subject.all).to match_array [] }
+    end
+  end
 end
