d5131bce7e5f877c0d9e0d16b242e09e3f0a7797
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-11 20:00:10 +0200

    Add a spec for mapping a valid customer attributes to a customer object

--- spec/mappers/customer_spec.rb
+++ spec/mappers/customer_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 require './lib/mappers/customer'
 
 describe Mappers::Customer do
+  let(:attrs) do
+  end
+  subject { described_class.new(attrs) }
+
+  describe '#customer' do
+    context 'when valid customer data is passed' do
+      specify { expect(subject.customer).to be_a Models::Customer }
+
+      it 'should map the customer basic attributes correctly' do
+        expect(subject.customer.user_id).to eq 1
+        expect(subject.customer.name).to eq 'Christina McArdle'
+      end
+    end
+  end
 end
