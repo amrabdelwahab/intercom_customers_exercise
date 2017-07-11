872653384df766c61d0b138877ba0fbd9e1a26c0
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-11 19:53:34 +0200

    Change the Customer model spec to change the Customer initialize method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,26 +2,21 @@ require 'spec_helper'
 require './lib/models/customer'
 
 describe Models::Customer do
-  let(:attrs) do
-    {
-      'latitude' => '52.986375',
-      'user_id' => 12,
-      'name' => 'Christina McArdle',
-      'longitude' => '-6.043701'
-    }
-  end
-  subject { described_class.new(attrs) }
+  let(:user_id) { 12 }
+  let(:name) { 'Christina McArdle' }
+  let(:location) { Models::Location.new('-6.043701', '52.986375') }
+  subject { described_class.new(user_id, name, location) }
 
   describe '#initialize' do
     specify { expect(subject.user_id).to eq 12 }
     specify { expect(subject.name).to eq 'Christina McArdle' }
-    specify { expect(subject.location).to be_a Models::Location }
+    specify { expect(subject.location).to eq location }
   end
 
   describe '#<=>' do
     context 'when two customers have the same user_id' do
-      let(:customer1) { described_class.new('user_id' => 12) }
-      let(:customer2) { described_class.new('user_id' => 12) }
+      let(:customer1) { described_class.new(12, 'User 1', location) }
+      let(:customer2) { described_class.new(12, 'User 2', location) }
 
       it 'should equate the two customers' do
         expect(customer1).to eq customer2
@@ -29,8 +24,8 @@ describe Models::Customer do
     end
 
     context 'when two customers have different user_id' do
-      let(:customer1) { described_class.new('user_id' => 1) }
-      let(:customer2) { described_class.new('user_id' => 12) }
+      let(:customer1) { described_class.new(1, 'User 1', location) }
+      let(:customer2) { described_class.new(2, 'User 2', location) }
 
       it 'should evaluate </> than of the customers based on the user_id' do
         expect(customer1 < customer2).to be_truthy
@@ -38,9 +33,9 @@ describe Models::Customer do
     end
 
     context 'when you have an array of customers' do
-      let(:customer1) { described_class.new('user_id' => 1) }
-      let(:customer2) { described_class.new('user_id' => 10) }
-      let(:customer3) { described_class.new('user_id' => 20) }
+      let(:customer1) { described_class.new(1, 'User 1', location) }
+      let(:customer2) { described_class.new(10, 'User 2', location) }
+      let(:customer3) { described_class.new(20, 'User 3', location) }
       let(:customers) { [customer2, customer1, customer3] }
 
       it 'should sort an array of customers based on user_id using #sort' do
