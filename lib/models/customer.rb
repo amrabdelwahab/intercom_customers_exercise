63df42f8ffa7a0c6874bd3dc0031c3c9f375a250
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-11 15:58:14 +0200

    Change the customer model to expect string keys attrs hash

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -5,9 +5,9 @@ module Models
     attr_reader :user_id, :name, :location
 
     def initialize(attrs)
-      @user_id = attrs[:user_id]
-      @name = attrs[:name]
-      @location = Location.new(attrs[:longitude], attrs[:latitude])
+      @user_id = attrs['user_id']
+      @name = attrs['name']
+      @location = Location.new(attrs['longitude'], attrs['latitude'])
     end
 
     def <=>(other)
