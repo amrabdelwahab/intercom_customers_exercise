589287e8b17a5ebbc061b4aa7d1cc34ee5661855
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-11 16:06:03 +0200

    Implement the customer repository all method
    * implement the private method #raw_customer_data which gets the raw customer data from the file_reader
    * implement the private method #map_customer which takes a customer hash and maps it to a customer object
    * implement permitted_params private method which white lists the params passed to a customer object
    
    *

--- lib/repositories/customer.rb
+++ lib/repositories/customer.rb
@@ -1,11 +1,31 @@
+require './lib/services/file_reader'
 module Repositories
   class Customer
+    WHITE_LISTED_PARAMS = %w[user_id name longitude latitude].freeze
+
     def initialize(source = './data/customers.json')
       @source = source
     end
 
     def all
-      @all ||= []
+      @all ||= raw_customers_data.map do |record_attrs|
+        map_customer(record_attrs)
+      end
+    end
+
+    private
+
+    def raw_customers_data
+      Services::FileReader.new(@source).json_array
+    end
+
+    def map_customer(attrs)
+      Models::Customer.new(permitted_params(attrs))
+    end
+
+    def permitted_params(attrs)
+      attrs
+        .select { |key, _| WHITE_LISTED_PARAMS.include? key }
     end
   end
 end
--- spec/respositories/customer_spec.rb
+++ spec/respositories/customer_spec.rb
@@ -14,5 +14,41 @@ describe Repositories::Customer do
     context 'when the file has no valid records' do
       specify { expect(subject.all).to match_array [] }
     end
+
+    context 'when the file has valid records' do
+      let(:customer_1_hash) do
+        {
+          'latitude' => '51.92893',
+          'user_id' => 1,
+          'name' => 'Alice Cahill',
+          'longitude' => '-10.27699'
+        }
+      end
+
+      let(:customer_2_hash) do
+        {
+          'latitude' => '51.8856167',
+          'user_id' => 2,
+          'name' => 'Ian McArdle',
+          'longitude' => '-10.4240951'
+        }
+      end
+
+      let(:customer_3_hash) do
+        {
+          'latitude' => '52.3191841',
+          'user_id' => 3,
+          'name' => 'Jack Enright',
+          'longitude' => '-8.5072391'
+        }
+      end
+      let(:json_data) do
+        [customer_1_hash, customer_2_hash, customer_3_hash]
+      end
+
+      specify { expect(subject.all).to be_an Array }
+      specify { expect(subject.all.count).to eq 3 }
+      specify { expect(subject.all).to all be_a Models::Customer }
+    end
   end
 end
