db3fcffeb7b0f978f4ad98b263c7111c4f47cdb7
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-12 12:47:02 +0200

    Implement the class distance_calculator to use the formulate described in this source https://en.wikipedia.org/wiki/Great-circle_distance#Radius_for_spherical_Earth

--- lib/services/distance_calculator.rb
+++ lib/services/distance_calculator.rb
@@ -1,12 +1,36 @@
 module Services
   class DistanceCalculator
+    attr_reader :source, :destination
+
+    EARTH_RADIUS = 6378.137
+
     def initialize(source, destination)
       @source = source
       @destination = destination
     end
 
     def distance_in_kms
-      0
+      @distance_in_kms ||= EARTH_RADIUS * central_angle
+    end
+
+    private
+
+    def central_angle
+      Math.acos(
+        product_of_sin_latitudes + (product_of_cos_latitudes * Math.cos(delta_longitudes))
+      )
+    end
+
+    def product_of_sin_latitudes
+      Math.sin(source.latitude_in_radians) * Math.sin(destination.latitude_in_radians)
+    end
+
+    def product_of_cos_latitudes
+      Math.cos(source.latitude_in_radians) * Math.cos(destination.latitude_in_radians)
+    end
+
+    def delta_longitudes
+      (source.longitude_in_radians - destination.longitude_in_radians).abs
     end
   end
 end
