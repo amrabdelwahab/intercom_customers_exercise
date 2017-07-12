a26783b6bfd07269c9793626a8a7f228c59751e9
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-12 11:21:21 +0200

    Add a spec for `Repositories::Customer#within_range` method with records scenario

--- spec/respositories/customer_spec.rb
+++ spec/respositories/customer_spec.rb
@@ -53,11 +53,57 @@ describe Repositories::Customer do
     end
 
     describe '#within_range' do
-      let(:reference_location) { Models::Location.new(53.3393, -6.2576841)}
+      let(:reference_location) { Models::Location.new(-6.2576841, 53.3393) }
       context 'when the data_source returns empty array of records' do
         specify do
           expect(subject.within_range(reference_location, 100))
             .to match_array []
+        end
+      end
+
+      context 'when the data_source returns some records' do
+        let(:customer_1_outside_range) do
+          {
+            'latitude' => '51.92893',
+            'user_id' => 1,
+            'name' => 'Alice Cahill',
+            'longitude' => '-10.27699'
+          }
+        end
+
+        let(:customer_2_outside_range) do
+          {
+            'latitude' => '51.8856167',
+            'user_id' => 2,
+            'name' => 'Ian McArdle',
+            'longitude' => '-10.4240951'
+          }
+        end
+
+        let(:customer_attrs_within_range) do
+          {
+            'latitude' => '52.3191841',
+            'user_id' => 4,
+            'name' => 'Ian Kehoe',
+            'longitude' => '-8.5072391'
+          }
+        end
+        let(:json_data) do
+          [customer_1_outside_range, customer_2_outside_range, customer_within_range]
+        end
+
+        it 'should include only the customer within_range' do
+          expect(subject.within_range(reference_location, 100))
+            .to be_an Array
+
+          expect(subject.within_range(reference_location, 100).count)
+            .to eq 1
+
+          expect(subject.within_range(reference_location, 100).first.user_id)
+            .to eq 4
+
+          expect(subject.within_range(reference_location, 100).first.name)
+            .to eq 'Ian Kehoe'
         end
       end
     end
