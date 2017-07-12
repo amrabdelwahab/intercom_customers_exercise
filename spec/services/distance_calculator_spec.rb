749f245cc2cf80095db17cce3a61c4208387ba87
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-12 13:36:37 +0200

    Refactor distance_calculator_spec to eliminate useless assignment

--- spec/services/distance_calculator_spec.rb
+++ spec/services/distance_calculator_spec.rb
@@ -3,21 +3,19 @@ require './lib/services/distance_calculator'
 
 describe Services::DistanceCalculator do
   let(:source) { Models::Location.new(-6.2576841, 53.3393) }
-  let(:destination) { Models::Location.new(-6.2576841, 53.3393) }
+  let(:destination) { Models::Location.new(-6.043701, 52.986375) }
 
   subject { described_class.new(source, destination) }
 
   describe '#distance_in_kms' do
-    let(:destination) { source }
-
     context 'when the source and destination are the same' do
+      let(:destination) { source }
       it 'should return 0' do
         expect(subject.distance_in_kms).to eq 0
       end
     end
 
     context 'when the source and destination are the same' do
-      let(:destination) { Models::Location.new(-6.043701, 52.986375) }
       it 'should return the correct distance in kms' do
         expect(subject.distance_in_kms).to eq 41.802589756143966
       end
